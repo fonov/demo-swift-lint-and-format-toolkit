c2ea24ac307a50460dc70c99c8c3592a18c50d20
Sergei Fonov <sergeufonov@gmail.com>
2022-11-29 17:24:25 +0400

    Initial Commit

---
+++ CleanCode/CleanCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCodeApp.swift
+//  CleanCode
+//
+//  Created by Sergei Fonov on 29.11.22.
+//
+
+import SwiftUI
+
+@main
+struct CleanCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
