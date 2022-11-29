be3a5100f3fd0d5f7fe06d7bc893354ed0701bfa
Sergei Fonov <sergeufonov@gmail.com>
2022-11-29 17:33:42 +0400

    add swiftlint and swiftformat

--- CleanCode/CleanCodeApp.swift
+++ CleanCode/CleanCodeApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct CleanCodeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
